2154ae68fd888be28dd87d3d18527a93ed3babea
owen970517 <tamia123123@naver.com>
2023-11-01 19:15:46 +0900

    rename: type 폴더 types로 변경

---
+++ src/types/userType.ts
@@ -0,0 +1,13 @@
+interface profileProps {
+    displayName? : string;
+}
+
+export interface IUser {
+    isLogin? :boolean;
+    displayName? :string ;
+    uid? : string;
+    updateProfile? : (props:profileProps) => void
+    userObj? : {
+        displayName? : string;
+    }
+}
---
+++ src/types/videoProps.ts
@@ -0,0 +1,33 @@
+export interface IVideo {
+    etag: string;
+    id? : string;
+    weeklyStatistics : {
+        viewCount : string;
+    };
+    snippet : {
+        title : string;
+        channelId : string;
+        channelTitle : string;
+        publishedAt : string;
+        resourceId : {
+            videoId : string;
+        }
+        thumbnails : {
+            medium : {
+                url : string
+            },
+            maxres : {
+                url : string
+            },
+            standard : {
+                url : string
+            }
+        }
+    },
+    contentDetails : {
+        duration : string
+    }
+    statistics : {
+        viewCount : string;
+    }
+}
