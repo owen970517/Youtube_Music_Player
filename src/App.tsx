eda343ac9465630930a288ed556310abfc65fe89
owen970517 <tamia123123@naver.com>
2023-11-01 19:20:59 +0900

    refactor: 불필요한 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,15 @@
 import { useEffect, useState } from 'react';
 import { BrowserRouter} from 'react-router-dom';
 import './App.css';
-import { authService} from './firebase';
 import Page from './pages/Page';
 import Header from './layout/Header';
-import { IUser } from './type/userType';
 import Footer from './layout/Footer';
 
 const App:React.FC= () => {
-  const [isLogin , setIsLogin] = useState(false);
-  const [userObj , setUserObj] = useState<IUser>();
-  useEffect(()=> {
-    authService.onAuthStateChanged((user)=> {
-        if(user) {
-            setIsLogin(true);
-            setUserObj({
-              displayName : user.displayName! ,
-              uid:user.uid,
-              updateProfile : (args:IUser) => user.updateProfile(args),
-            })
-        }else{
-            setIsLogin(false)
-        }
-    })
-},[])
-const refreshUser = () => {
-  const user = authService.currentUser;
-  if (user) {
-    setUserObj({
-      displayName : user.displayName!,
-      uid : user.uid,
-      updateProfile : (args) => user.updateProfile(args),
-    })
-  }
-}
   return (
     <BrowserRouter>
-      <Header isLogin={isLogin} userObj={userObj}/>
-      <Page userObj={userObj} refreshUser={refreshUser}/>
+      <Header />
+      <Page />
       <Footer/>
     </BrowserRouter>
   );
