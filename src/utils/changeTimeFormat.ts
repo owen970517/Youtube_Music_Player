b77c83773deee8ef8d4510c21b1e2bc5829db6b5
owen970517 <tamia123123@naver.com>
2023-11-16 19:03:53 +0900

    refactor: dayjs의 duration 사용 코드 수정

--- src/utils/changeTimeFormat.ts
+++ src/utils/changeTimeFormat.ts
@@ -1,4 +1,6 @@
 import dayjs from "dayjs";
+import duration from 'dayjs/plugin/duration';
+dayjs.extend(duration);
 
 export const formDuration = (value:string) => {
     const timeDuration = dayjs.duration(value);
