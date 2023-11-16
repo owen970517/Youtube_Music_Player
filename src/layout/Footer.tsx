0e814e4ed5b62ee39a3250dd9461108e8f6218ae
owen970517 <tamia123123@naver.com>
2023-11-16 19:13:11 +0900

    design: 푸터 디자인 크기 수정

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -4,18 +4,18 @@ import FooterImg from '../data/하단 Tam logo.png'
 const Footer = () => {
   return (
     <FooterDiv>
-        <Img src={FooterImg} alt='footer'/>
+      <Img src={FooterImg} alt='footer'/>
     </FooterDiv>
   )
 }
 const FooterDiv = styled.footer`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 20px 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
 `
 const Img = styled.img`
-  width: 100px;
-  height: 100px;
+  width: 90px;
+  height: 90px;
 `
 export default Footer
