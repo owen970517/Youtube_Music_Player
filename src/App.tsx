3342b058a65ef25cb49d7230b874295c2b69e516
owen970517 <tamia123123@naver.com>
2023-12-15 18:26:53 +0900

    refactor: 새로고침 시 데이터가 초기화되는 현상 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,34 @@ import './App.css';
 import Page from './pages/Page';
 import Header from './layout/Header';
 import Footer from './layout/Footer';
+import { getFriaPlaylistInfo, getFriaPlaylists, playlistActions } from './store/playlistSlice';
+import { IVideo } from './types/videoProps';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from './store/store';
 
 const App:React.FC= () => {
+  const dispatch = useDispatch<AppDispatch>()
+  const {allData,coverVideo} = useSelector((state:RootState) => state.playlist)
+  const formatIdString = (list:IVideo[]) => {
+    return list?.map((x) => "&id=" + x.snippet.resourceId.videoId).join("");
+  }
+  const friaPlaylistId = formatIdString(allData)
+
+  useEffect(()=> {
+    dispatch(getFriaPlaylists())
+    dispatch(getFriaPlaylistInfo(friaPlaylistId))
+  },[dispatch, friaPlaylistId])
+
+  useEffect(() => {
+    let list:IVideo[] = []
+    if (coverVideo ) {
+      list = [...coverVideo]  
+      list.sort((a:IVideo,b:IVideo) => parseInt(b.statistics.viewCount)-parseInt(a.statistics.viewCount))  
+    }
+    dispatch(playlistActions.setAllVideos(list))
+    dispatch(playlistActions.setFilteredVideos(list))
+  },[coverVideo, dispatch])
   return (
     <BrowserRouter>
       <Header />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,36 +1,10 @@
-import { useEffect } from 'react';
 import React from 'react';
 import styled from "styled-components"
-import { useDispatch, useSelector } from 'react-redux';
-import { getFriaPlaylistInfo, getFriaPlaylists, playlistActions } from '../store/playlistSlice';
-import { AppDispatch, RootState } from '../store/store';
 import HomeImg from '../data/main.png'
 import LatestMusics from 'src/components/musics/LatestMusics';
 import Charts from 'src/components/Charts';
-import { IVideo } from 'src/types/videoProps';
 
 function Home() {
-  const dispatch = useDispatch<AppDispatch>()
-  const {allData,coverVideo} = useSelector((state:RootState) => state.playlist)
-  const formatIdString = (list:IVideo[]) => {
-    return list?.map((x) => "&id=" + x.snippet.resourceId.videoId).join("");
-  }
-  const friaPlaylistId = formatIdString(allData)
-
-  useEffect(()=> {
-    dispatch(getFriaPlaylists())
-    dispatch(getFriaPlaylistInfo(friaPlaylistId))
-  },[dispatch, friaPlaylistId])
-
-  useEffect(() => {
-    let list:IVideo[] = []
-    if (coverVideo ) {
-      list = [...coverVideo]  
-      list.sort((a:IVideo,b:IVideo) => parseInt(b.statistics.viewCount)-parseInt(a.statistics.viewCount))  
-    }
-    dispatch(playlistActions.setAllVideos(list))
-    dispatch(playlistActions.setFilteredVideos(list))
-  },[coverVideo, dispatch])
   return (
     <Main>
       <ImgWrapper>
