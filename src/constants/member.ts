a670fc3fa18141d6329d43d7c5b82abd6fd59541
owen970517 <tamia123123@naver.com>
2024-04-08 17:15:44 +0900

    refactor: 상수 변수 변경

--- src/constants/member.ts
+++ src/constants/member.ts
@@ -1,7 +1,6 @@
 export const Members = [
-    { name: '전체', value: '', altValue: '' },
-    { name: '고여름', value: '고여름', altValue: 'GoSummer' },
-    { name: '바밍', value: '바밍', altValue: 'Bombing' },
-    { name: '베베리', value: '베베리', altValue:'Beberry'},
-    { name:'블러비' ,value:'블러비' ,altValue:'Blovey'}
-  ]
+  { name: '전체', value: '', altValue: '' },
+  { name: '고여름', value: '고여름', altValue: 'GoSummer' },
+  { name: '바밍', value: '바밍', altValue: 'Bombing' },
+  { name: '베베리', value: '베베리', altValue:'Beberry'},
+]
